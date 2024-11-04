7b6ba89cf0a940d699813947c71e4e7170e135b3
Matias G Adelina <98082081+matias2k2@users.noreply.github.com>
2024-11-04 03:54:10 -0300

    retomando
    Retomando o trabalho de Aplicacao Movel

---
+++ binaJC/src/main/java/tinario9954/gmail/com/binaJC/BinaJcApplication.java
@@ -0,0 +1,13 @@
+package tinario9954.gmail.com.binaJC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaJcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaJcApplication.class, args);
+	}
+
+}
---
+++ binaJC/src/test/java/tinario9954/gmail/com/binaJC/BinaJcApplicationTests.java
@@ -0,0 +1,13 @@
+package tinario9954.gmail.com.binaJC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaJcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
